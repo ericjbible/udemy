2309203cb17067d814f6a298bff7a1e1d32ac8ce
Eric Bible <eric@ejbdevdes.com>
2023-12-18 15:13:11 -0500

    resolution to assignment #2 Angular with Max

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username = '';
+  username1 = '';
+  onUpdateUsername() {
+    this.username1 = this.username;
+    return this.username = '';
+  }
 }
