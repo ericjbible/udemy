81f801fdc3727f97c7c99cc92c25a99c950cf090
Eric Bible <eric@ejbdevdes.com>
2023-12-20 14:04:15 -0500

    setup click counter array functionality with Max

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -3,15 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   buttonClick = false;
+  arrayClickCount = 0;
+  clickArray = [];
 
   onDisplayHidden() {
-    if (this.buttonClick === false)
+    if (this.buttonClick === false) {
       this.buttonClick = true;
-    else if (this.buttonClick === true)
+    } else if (this.buttonClick === true) {
       this.buttonClick = false;
+    }
+    this.arrayClickCount = this.arrayClickCount + 1;
+
+    this.clickArray.push(this.arrayClickCount);
+    console.log(this.clickArray);
   }
 }
